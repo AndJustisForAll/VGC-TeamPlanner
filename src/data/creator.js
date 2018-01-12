40d50a64ccf05265b445ef187834f7db629adc95
amunoz <amunozdv@gmail.com>
2018-01-12 10:44:26 -0600

    json pkdex feed creator

---
+++ src/data/creator.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var request = require('sync-request');
+
+var allPokemon = [];
+const latestPokemonIndex = 722;
+for (let i = 1; i < latestPokemonIndex; i++) {
+    const res = request('GET', `http://pokeapi.co/api/v2/pokemon/${i}/`, {
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    });
+    const body = JSON.parse(res.getBody('utf-8'));
+    let pokemon = {
+        id: body.id,
+        name: body.name,
+        types: body.types.map((type) => type.type.name)
+    };
+    allPokemon[i] = pokemon;
+    console.log(`Pokemon ${pokemon.name} completed`);
+}
+
+var text = JSON.stringify(allPokemon);
+fs.writeFile("/home/angel/Documents/test.json", text, function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
